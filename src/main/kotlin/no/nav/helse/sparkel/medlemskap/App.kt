22a1afe95715e02185c549fe09978159fd4a50ab
David Steinsland <david@steinsland.email>
2020-08-12 14:30:05 +0200

    endrer namespace til medlemskap

--- src/main/kotlin/no/nav/helse/sparkel/medlemskap/App.kt
+++ src/main/kotlin/no/nav/helse/sparkel/medlemskap/App.kt
@@ -7,7 +7,7 @@ import java.nio.file.Paths
 fun main() {
     val env = System.getenv()
     RapidApplication.create(env).apply {
-        val client = MedlemskapClient("http://medlemskap-oppslag.default.svc.nais.local", AzureClient(
+        val client = MedlemskapClient("http://medlemskap-oppslag.medlemskap.svc.nais.local", AzureClient(
             tenantUrl = "${env.getValue("AZURE_TENANT_BASEURL")}/${env.getValue("AZURE_TENANT_ID")}",
             clientId = "/var/run/secrets/nais.io/azuread/client_id".readFile() ?: env.getValue("AZURE_CLIENT_ID"),
             clientSecret = "/var/run/secrets/nais.io/azuread/client_secret".readFile() ?: env.getValue("AZURE_CLIENT_SECRET")
